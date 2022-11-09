b8e17e6e16aaa3af1d0d021c45ae4b35974fd716
jkaar <96846917+jkaar@users.noreply.github.com>
2022-11-09 17:42:49 +0000

    begun code to gather data

---
+++ get_data.py
@@ -0,0 +1,6 @@
+import cv2
+import os
+import numpy as np
+
+dir_training = "C:/Users/Joshua/Desktop/Projects/Python/osu-ai/osu-ai/trainingdata.npy"
+dir_target = "C:/Users/Joshua/Desktop/Projects/Python/osu-ai/osu-ai/targetdata.npy"
