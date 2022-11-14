82959b8f43b001db5849101b7da9fca7c6c24ae5
Josh <joshuamyers1097@gmail.com>
2022-11-14 13:57:55 +0000

    cleaned up code to improve speed

--- utils/templateMatch.py
+++ utils/templateMatch.py
@@ -4,8 +4,10 @@ import numpy as np
 
 # get image data from getData and pass through into this function to get coordinates
 def temp_match():
-    cursor_img = cv2.imread('C:/Users/Joshua/Desktop/Projects/Python/osu-ai/osu-ai/template-matching/cursor.png', cv2.IMREAD_UNCHANGED)
-    game_img = cv2.imread('C:/Users/Joshua/Desktop/Projects/Python/osu-ai/osu-ai/template-matching/example.png', cv2.IMREAD_UNCHANGED)
+    # cursor image is read converted to grayscale after reading to match with the image passed into the function
+    cursor_img = cv2.imread('C:/Users/joshu/Desktop/Projects/Python/osu-ai/template-matching/cursor.png', cv2.IMREAD_UNCHANGED)
+    game_img = cv2.imread('C:/Users/joshu/Desktop/Projects/Python/osu-ai/template-matching/example.png', cv2.IMREAD_UNCHANGED)
+    #game_img = input_img
 
     #cv2.imshow('Game', game_img)
     #cv2.waitKey()
@@ -21,30 +23,13 @@ def temp_match():
     w = cursor_img.shape[1]
     h = cursor_img.shape[0]
 
-    cv2.rectangle(game_img, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
-
-    threshold = .355
-
-    yloc, xloc = np.where(result >= threshold)
-
-    for (x, y) in zip(xloc, yloc):
-        cv2.rectangle(game_img, (x, y), (x + w, y + h), (0,255,255), 2)
-
-    #cv2.imshow('Game', game_img)
-    #cv2.waitKey()
-    #cv2.destroyAllWindows()
-
-    rectangles = []
-    for (x, y) in zip(xloc, yloc):
-        rectangles.append([int(x), int(y), int(w), int(h)])
-        rectangles.append([int(x), int(y), int(w), int(h)])
-
-    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
-
     # in openCV the y coordinate goes down, i.e in a resolution of 800x600, 
     # the coordinate (300, 200), is actually (300, 400)
-    x = rectangles[0][0] + 0.5 * rectangles[0][2]
-    y = 600 - (rectangles[0][1] + 0.5 * rectangles[0][3])
+    
+    x = max_loc[0] + 0.5 * w
+    y = 600 - (max_loc[1] + 0.5 * h)
+
+    # returns tuple containing coordinates
     return (x,y)
 
 print(temp_match())
