50ab03aa16613ce0bf8df6dcb22f7f12938b1960
jkaar <96846917+jkaar@users.noreply.github.com>
2022-11-23 00:20:41 +0000

    keypoint creating function

--- train.py
+++ train.py
@@ -1,4 +1,53 @@
 # training model with the data previously created
-# three options: pytorch, pytorch and fast ai, or tensorflow and keras
-import numpy as np
-import torch
+# tensorflow and keras will be used for this project
+from fastai.vision.all import *
+import time
+import cv2
+from utils.getScreen import grab_screen
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, CuDNNLSTM as LSTM, Input, GaussianNoise
+
+path = Path("C:\Users\Joshua\Desktop\Data\osu-ai")
+imgs = get_image_files(path)
+lbls = get_files(path, extension='.png')
+print(f"Total images: {len(imgs)}")
+
+def img2kpts(f):
+    name = os.path.basename(f)
+    split = str(name).split("-")
+    kpts = []
+    kpts.append([int(split[1]), int(split[2][:4])])
+    return tensor(kpts)
+
+def label_func(x): return x.name[0:7]
+
+def run():
+    path = Path("C:/Users/Joshua/Desktop/Data/osu-ai/")
+    fnames = get_image_files(path)
+    print(f"Total Images:{len(fnames)}")
+
+
+    dls = ImageDataLoaders.from_path_func(path, fnames, label_func,bs=20, num_workers=0)
+    learn = cnn_learner(dls, resnet18, metrics=error_rate)
+    print("Loaded")
+    learn.remove_cb(ProgressCallback)
+    learn.fine_tune(2, base_lr=1.0e-02)
+
+    learn.export()
+
+    # start_time = time.time()
+    # test = learn.predict('g1-j5.png')
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    # print(test)
+    #start_time = time.time()
+    #image = grab_screen(region=(50, 100, 799, 449))
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #image = cv2.Canny(image, threshold1=200, threshold2=300)
+    #image = cv2.resize(image, (224, 224))
+    #test = learn.predict(image)
+    #print("--- %s seconds ---" % (time.time() - start_time))
+    #print(test)
+
+
+if __name__ == '__main__':
+    run()
