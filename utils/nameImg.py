54a0ae2b85624f97bf2a338c8f2affa92d0a1396
Josh <joshuamyers1097@gmail.com>
2022-12-23 01:55:26 +0000

    csv and image creation in single file, cleaned up

--- utils/nameImg.py
+++ utils/nameImg.py
@@ -1,6 +1,9 @@
+# this now functions to create the images as well as creating a
+# csv file with the equivalent coordinates for the 80x60 image
 import cv2
 import numpy as np
 import os
+import pandas as pd
 
 data = np.load("./data/training_data.npy", allow_pickle=True)
 targets = np.load("./data/target_data.npy", allow_pickle=True)
@@ -16,11 +19,17 @@ for i, image in enumerate(data):
 
 count = 0
 
+# Below code creates the dataframe and following that, the csv file
+cols = ['x', 'y', 'image']
+lst = []
+for data in holder_list:
+    lst.append([int(data[1][0]/10), int(data[1][1]/10), data[0]])
+
+df = pd.DataFrame(lst, columns=cols)
+df.to_csv('resizedData.csv',index=False)
+
+# Code to create and name the images from the numpy arrays
 os.chdir('C:/Users/joshu/Desktop/osu-ai-data/')
 for data in holder_list:
-    #print(int(data[1][0]))
     count += 1
-    #if not os.path.exists(f"{int(data[1][0])}-{int(data[1][1])}"):
-    #    os.mkdir(f"{int(data[1][0])}-{int(data[1][1])}")
-    #cv2.imwrite(f"C:/Users/Joshua/Desktop/Data/osu-ai/{int(data[1][0])}-{int(data[1][1])}/{count}.png", data[0]) 
-    cv2.imwrite(f"C:/Users/joshu/Desktop/osu-ai-data/{int(data[1][0])}-{int(data[1][1])}-{count}.png", data[0]) 
+    cv2.imwrite(f"C:/Users/joshu/Desktop/osu-ai-data/{count}-{int(data[1][0])}-{int(data[1][1])}.png", data[0]) 
